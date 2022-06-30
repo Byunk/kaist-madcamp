b2da7a100abaca64b10c66494bf171ea55fe7545
corgiLoaf <keem1031@naver.com>
2022-06-30 21:32:28 +0900

    viewpager2 apply tap implement

--- app/src/main/java/com/example/cs496_pj1/MainActivity.kt
+++ app/src/main/java/com/example/cs496_pj1/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.example.cs496_pj1.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import java.util.ArrayList
 
 
 private const val NUM_PAGES = 3
@@ -25,31 +26,71 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         viewPager = findViewById(R.id.pager)
         val pagerAdapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
         viewPager.adapter = pagerAdapter
-
 
         val tabLayout = binding.tabs
 
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = tabTitleArray[position]
         }.attach()
+
+        setUpTap()
     }
 
-    private inner class ViewPagerAdapter(fm: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fm, lifecycle) {
+    private inner class ViewPagerAdapter(fm: FragmentManager, lifecycle: Lifecycle) :
+        FragmentStateAdapter(fm, lifecycle )
+    {
 
         override fun getItemCount(): Int = NUM_PAGES
+
+        private val mFragmentList = ArrayList<Fragment>()
+        private val mFragmentTitleList = ArrayList<String>()
 
         override fun createFragment(position: Int): Fragment {
             when (position) {
                 0 -> return ContactsFragment()
                 1 -> return GalleryFragment()
+                2 -> return tmp()
             }
             return ContactsFragment()
         }
+
+//        fun getItem(position: Int): Fragment {
+//            return mFragmentList[position]
+//        }
+//
+//        fun getPageTitle(position: Int): CharSequence? {
+//            return mFragmentTitleList[position]
+//        }
+//
+//        fun addFragment(fragment: Fragment, title: String){
+//            mFragmentList.add(fragment)
+//        }
+
+    }
+
+    private fun setUpTap(){
+        val adapter = ViewPagerAdapter(supportFragmentManager,lifecycle )
+        adapter.createFragment(0)
+        adapter.createFragment(1)
+        adapter.createFragment(2)
+        binding.pager.adapter = adapter
+
+        TabLayoutMediator(binding.tabs, viewPager) { tab, position ->
+            tab.text = tabTitleArray[position]
+        }.attach()
+
+
+
+        binding.tabs.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_contact_phone_24)
+        binding.tabs.getTabAt(1)!!.setIcon(R.drawable.ic_baseline_photo_library_24)
+        binding.tabs.getTabAt(2)!!.setIcon(R.drawable.ic_baseline_assignment_24)
+
 
     }
 }
--- app/src/main/java/com/example/cs496_pj1/tmp.kt
+++ app/src/main/java/com/example/cs496_pj1/tmp.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.cs496_pj1.R
 
-class GalleryFragment : Fragment() {
+class tmp : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +19,7 @@ class GalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false)
+        return inflater.inflate(R.layout.fragment_todo, container, false)
     }
 
 }
