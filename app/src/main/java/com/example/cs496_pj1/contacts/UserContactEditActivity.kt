e20f5ca98971215ccf57a352abc5d42c912fb7c9
Byunk <clearman001@kaist.ac.kr>
2022-06-30 22:13:30 +0900

    -Add Edit Activity

---
+++ app/src/main/java/com/example/cs496_pj1/contacts/UserContactEditActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cs496_pj1.contacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserContactEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_contact_edit)
+    }
+}
